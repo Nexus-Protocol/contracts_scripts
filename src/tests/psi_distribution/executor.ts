4f5bed7f3a29e79048b055f7f96ec62ff9831373
1Zaitsev <4yu@bk.ru>
2021-10-26 00:23:24 +0300

    get parent branch changes

--- src/tests/psi_distribution/executor.ts
+++ src/tests/psi_distribution/executor.ts
@@ -3,7 +3,7 @@ import {
 	send_tokens_and_distribute
 } from "./definition";
 import { readFileSync } from 'fs';
-import {Command} from 'commander';
+import {Command, option} from 'commander';
 import {get_lcd_config_with_wallet, LCDConfig} from '../../utils';
 import {BlockTxBroadcastResult, getContractEvents} from "@terra-money/terra.js";
 
@@ -34,31 +34,7 @@ async function run(config_path: string) {
 	const config: Config = JSON.parse(readFileSync(config_path, 'utf-8'))
 	const [lcd_client, sender] = await get_lcd_config_with_wallet(config.lcd_client);
 
-	// //get deployed psi_distributor_addr
-	// const psi_distributor_addr = await psi_distributor_init(lcd_client, sender);
-	// console.log(`psi_distributor_addr: ${JSON.stringify(psi_distributor_addr)}`);
-	//
-	// //get deployed psi_distributor_config
-	// const psi_distributor_config = await query_config(lcd_client, psi_distributor_addr);
-	// console.log(`psi_distributor_config: ${psi_distributor_config}`);
-
-	const psi_distributor_deployment_result = await psi_distributor_init(lcd_client, sender);
-	console.log(`psi_distributor_addr: ${JSON.stringify(psi_distributor_deployment_result)}`);
-
-	//
-	const psi_distribution_response =  await send_tokens_and_distribute(
-		lcd_client,
-		sender,
-		psi_distributor_deployment_result.psi_distributor_config.psi_token_addr,
-		psi_distributor_deployment_result.psi_distributor_addr,
-		1000);
-	console.log(`psi_distribution_response: ${JSON.stringify(psi_distribution_response)}`);
-
-	// const mint_response = await mint(lcd_client, sender, psi_distributor_deployment_result.psi_distributor_config.psi_token_addr, psi_distributor_deployment_result.psi_distributor_addr);
-	// console.log(`-------------- : ${JSON.stringify(mint_response)}`);
-	//
-	// const distribution_result = await distribute(lcd_client, sender, psi_distributor_addr);
-	// console.log(`-------------- : ${JSON.stringify(distribution_result)}`);
+	await psi_distributor_init(lcd_client, sender);
 }
 
 run_program()
