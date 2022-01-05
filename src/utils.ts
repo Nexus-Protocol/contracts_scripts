e265efcb288ac4fd85cab6dae39225a4fc6b16d7
1Zaitsev <4yu@bk.ru>
2022-01-05 19:41:16 +0300

    anchor deployment merged

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,30 @@
-import { readFileSync } from 'fs';
-import {BlockTxBroadcastResult, Coin, Coins, getCodeId, getContractAddress, getContractEvents, LCDClient, LocalTerra, MnemonicKey, Msg, MsgExecuteContract, MsgInstantiateContract, MsgStoreCode, StdFee, Wallet} from '@terra-money/terra.js';
+import {readFileSync} from 'fs';
+import {
+	BlockTxBroadcastResult,
+	Coin,
+	Coins,
+	getCodeId,
+	getContractAddress,
+	getContractEvents,
+	LCDClient,
+	LocalTerra,
+	MnemonicKey,
+	Msg,
+	MsgExecuteContract,
+	MsgInstantiateContract,
+	MsgStoreCode,
+	StdFee,
+	Wallet
+} from '@terra-money/terra.js';
 import {BassetVaultConfig} from './config';
 import {SecretsManager} from 'aws-sdk';
 import * as prompt from 'prompt';
 import {isTxSuccess} from './transaction';
 
-export async function create_contract(lcd_client: LCDClient, sender: Wallet, contract_name: string, wasm_path: string, init_msg: object): Promise<string> {
+export async function create_contract(lcd_client: LCDClient, sender: Wallet, contract_name: string, wasm_path: string, init_msg: object, init_funds?: Coin[]): Promise<string> {
 	let code_id = await store_contract(lcd_client, sender, wasm_path);
 	console.log(`${contract_name} uploaded\n\tcode_id: ${code_id}`);
-	let contract_addr = await instantiate_contract(lcd_client, sender, sender.key.accAddress, code_id, init_msg);
+	let contract_addr = await instantiate_contract(lcd_client, sender, sender.key.accAddress, code_id, init_msg, init_funds);
 	console.log(`${contract_name} instantiated\n\taddress: ${contract_addr}`);
 	return contract_addr;
 }
@@ -31,13 +47,13 @@ export async function store_contract(lcd_client: LCDClient, sender: Wallet, wasm
 	}
 }
 
-async function instantiate_contract_raw(lcd_client: LCDClient, sender: Wallet, admin: string, code_id: number, init_msg: object, init_funds?: Coin[]): Promise<BlockTxBroadcastResult> {
+export async function instantiate_contract_raw(lcd_client: LCDClient, sender: Wallet, admin: string, code_id: number, init_msg: object, init_funds?: Coin[]): Promise<BlockTxBroadcastResult> {
 	const messages: Msg[] = [new MsgInstantiateContract(
 		sender.key.accAddress,
-		 	admin,
-			code_id,
-			init_msg,
-			init_funds
+		admin,
+		code_id,
+		init_msg,
+		init_funds
 	)];
 
 	while (true) {
@@ -55,11 +71,12 @@ export async function instantiate_contract(lcd_client: LCDClient, sender: Wallet
 	return getContractAddress(result)
 }
 
-export async function execute_contract(lcd_client: LCDClient, sender: Wallet, contract_addr: string, execute_msg: object): Promise<BlockTxBroadcastResult | undefined> {
+export async function execute_contract(lcd_client: LCDClient, sender: Wallet, contract_addr: string, execute_msg: object, coins?: Coin[]): Promise<BlockTxBroadcastResult | undefined> {
 	const messages: Msg[] = [new MsgExecuteContract(
 		sender.key.accAddress,
 		contract_addr,
-		execute_msg
+		execute_msg,
+		coins
 	)];
 	let result = await send_message(lcd_client, sender, messages);
 	return result
@@ -149,7 +166,8 @@ export interface BassetVaultInfo {
 	nasset_token_config_holder_addr: string,
 	nasset_token_addr: string,
 	nasset_token_rewards_addr: string,
-	psi_distributor_addr: string
+	psi_distributor_addr: string,
+	nasset_psi_swap_contract_addr: string,
 }
 
 export async function init_basset_vault(lcd_client: LCDClient, sender: Wallet, code_id: number, init_msg: BassetVaultConfig): Promise<BassetVaultInfo> {
@@ -161,7 +179,8 @@ export async function init_basset_vault(lcd_client: LCDClient, sender: Wallet, c
 		nasset_token_addr: '',
 		nasset_token_config_holder_addr: '',
 		nasset_token_rewards_addr: '',
-		psi_distributor_addr: ''
+		psi_distributor_addr: '',
+		nasset_psi_swap_contract_addr: ''
 	};
 	let contract_events = getContractEvents(init_contract_res);
 	for (let contract_event of contract_events) {
@@ -185,6 +204,10 @@ export async function init_basset_vault(lcd_client: LCDClient, sender: Wallet, c
 			basset_vault_info.psi_distributor_addr = psi_distributor_addr;
 		}
 
+		let nasset_psi_swap_contract_addr = contract_event["nasset_psi_swap_contract_addr"];
+		if (nasset_psi_swap_contract_addr !== undefined) {
+			basset_vault_info.nasset_psi_swap_contract_addr = nasset_psi_swap_contract_addr;
+		}
 	}
 	return basset_vault_info;
 }
@@ -212,7 +235,6 @@ export async function calc_fee_and_send_tx(lcd_client: LCDClient, sender: Wallet
 		return undefined;
 	}
 }
-
 
 async function get_tx_fee(lcd_client: LCDClient, sender: Wallet, msgs: Msg[], tax?: Coin[]): Promise<StdFee | undefined> {
 	try {
@@ -299,8 +321,6 @@ export interface LCDConfig {
 	localterra: boolean,
 	url?: string,
 	chain_id?: string,
-	account?: number,
-	index?: number,
 	aws_secrets?: AwsSecrets
 }
 
@@ -316,15 +336,6 @@ function check_non_localterra(lcd_config: LCDConfig) {
 export async function get_lcd_config_with_wallet(lcd_config: LCDConfig): Promise<[LCDClient, Wallet]> {
 	let lcd_client: LCDClient;
 	let sender: Wallet;
-	let account_id = lcd_config.account;
-	if (account_id === undefined) {
-		account_id = 0;
-	}
-	let index_id = lcd_config.index;
-	if (index_id === undefined) {
-		index_id = 0;
-	}
-
 	if (lcd_config.localterra) {
 		const localterra = new LocalTerra()
 		lcd_client = localterra;
@@ -343,7 +354,7 @@ export async function get_lcd_config_with_wallet(lcd_config: LCDConfig): Promise
 			process.exit(1);
 		}
 
-		const owner = new MnemonicKey({mnemonic: seed, account: account_id, index: index_id});
+		const owner = new MnemonicKey({mnemonic: seed});
 		sender = new Wallet(lcd_client, owner);
 	} else {
 		check_non_localterra(lcd_config);
@@ -352,7 +363,7 @@ export async function get_lcd_config_with_wallet(lcd_config: LCDConfig): Promise
 			chainID: lcd_config.chain_id!
 		});
 		const seed = await prompt_for_seed();
-		const owner = new MnemonicKey({mnemonic: seed, account: account_id, index: index_id});
+		const owner = new MnemonicKey({mnemonic: seed});
 		sender = new Wallet(lcd_client, owner);
 	}
 
