07e6a8adf15ca02a5e2b254a53b5ff05158fa272
1Zaitsev <4yu@bk.ru>
2022-01-06 03:38:59 +0300

    in progress

--- src/integration_tests/deploy_mirror/config.ts
+++ src/integration_tests/deploy_mirror/config.ts
@@ -1,5 +1,13 @@
 import {TokenConfig} from "../../config";
 
+export type Addr = string;
+export type Decimal = string;
+export type Uint128 = string;
+
+export type u64 = number;
+
+//=====================================================================================================================
+
 export function MirrorTokenConfig(minter: string): TokenConfig {
     return {
         name: "Mirror Governance Token",
@@ -12,14 +20,259 @@ export function MirrorTokenConfig(minter: string): TokenConfig {
     }
 }
 
-export function aUSTConfig(minter: string): TokenConfig {
+//=====================================================================================================================
+
+export interface MirrorGovConfig {
+    mirror_token: Addr,
+    quorum: Decimal,
+    threshold: Decimal,
+    voting_period: u64,
+    effective_delay: u64,
+    proposal_deposit: Uint128,
+    voter_weight: Decimal,
+    snapshot_period: u64,
+}
+
+export function MirrorGovConfig(
+    mirror_token: Addr,
+): MirrorGovConfig {
     return {
-        name: "Anchor Terra USD",
-        symbol: "aUST",
-        decimals: 6,
-        initial_balances: [],
-        mint: {
-            minter: minter,
-        }
+        mirror_token: mirror_token,
+        quorum: "0.1",
+        threshold: "0.5",
+        voting_period: 201600,
+        effective_delay: 100800,
+        proposal_deposit: "100000000",
+        voter_weight: "1",
+        snapshot_period: 14400,
+    }
+}
+
+//=====================================================================================================================
+
+export interface MirrorCommunityConfig {
+    owner: Addr,            // mirror gov contract for prod, but my wallet for tests
+    mirror_token: Addr,
+    spend_limit: Uint128,
+}
+
+export function MirrorCommunityConfig(
+    owner: Addr,
+    mirror_token: Addr,
+): MirrorCommunityConfig {
+    return {
+        owner: owner,
+        mirror_token: mirror_token,
+        spend_limit: "50000000000000",
+    }
+}
+
+//=====================================================================================================================
+
+export interface MirrorCollectorConfig {
+    owner: Addr,    // mirror_factory
+    distribution_contract: Addr, // collected rewards receiver(mirror_gov)
+    terraswap_factory: Addr,
+    mirror_token: Addr,
+    base_denom: string,
+    // aUST params
+    aust_token: Addr,
+    anchor_market: Addr,
+    // bLuna params
+    bluna_token: Addr,
+    bluna_swap_denom: string,
+}
+
+export function MirrorCollectorConfig(
+    owner: Addr,
+    distribution_contract: Addr,
+    terraswap_factory: Addr,
+    mirror_token: Addr,
+    aust_token: Addr,
+    anchor_market: Addr,
+    bluna_token: Addr,
+): MirrorCollectorConfig {
+    return {
+        owner: owner,
+        distribution_contract: distribution_contract,
+        terraswap_factory: terraswap_factory,
+        mirror_token: mirror_token,
+        base_denom: "uusd",
+        aust_token: aust_token,
+        anchor_market: anchor_market,
+        bluna_token: bluna_token,
+        bluna_swap_denom: "uusd",
+    }
+}
+
+//=====================================================================================================================
+
+export interface MirrorOracleConfig {
+    owner: Addr,    //mirror_factory
+    base_asset: string,
+}
+
+export function MirrorOracleConfig(owner: Addr): MirrorOracleConfig {
+    return {
+        owner: owner,
+        base_asset: "uusd"
+    }
+}
+
+//=====================================================================================================================
+
+export interface MirrorMintConfig {
+    owner: Addr, //mirror_factory
+    oracle: Addr,
+    collector: Addr,
+    collateral_oracle: Addr,
+    staking: Addr,
+    terraswap_factory: Addr,
+    lock: Addr,
+    base_denom: string,
+    token_code_id: u64,
+    protocol_fee_rate: Decimal,
+}
+
+export function MirrorMintConfig(
+    owner: Addr, //mirror_factory
+    oracle: Addr,
+    collector: Addr,
+    collateral_oracle: Addr,
+    staking: Addr,
+    terraswap_factory: Addr,
+    lock: Addr,
+    token_code_id: u64,
+): MirrorMintConfig {
+    return {
+        owner: owner,
+        oracle: oracle,
+        collector: collector,
+        collateral_oracle: collateral_oracle,
+        staking: staking,
+        terraswap_factory: terraswap_factory,
+        lock: lock,
+        base_denom: "uusd",
+        token_code_id: token_code_id,
+        protocol_fee_rate: "0.15",
+    }
+}
+
+//=====================================================================================================================
+
+export interface MirrorLockConfig {
+    owner: Addr,
+    mint_contract: Addr,
+    base_denom: string,
+    lockup_period: u64,
+}
+
+export function MirrorLockConfig(
+    owner: Addr,
+    mint_contract: Addr,
+): MirrorLockConfig {
+    return {
+        owner: owner,
+        mint_contract: mint_contract,
+        base_denom: "uusd",
+        lockup_period: 1209600,
+    }
+}
+
+//=====================================================================================================================
+
+export interface MirrorStakingConfig {
+    owner: Addr,    //mirror_factory
+    mirror_token: Addr,
+    mint_contract: Addr,
+    oracle_contract: Addr,
+    terraswap_factory: Addr,
+    base_denom: string,
+    premium_min_update_interval: u64,
+    short_reward_contract: string,
+}
+
+export function MirrorStakingConfig(
+    owner: Addr,    //mirror_factory
+    mirror_token: Addr,
+    mint_contract: Addr,
+    oracle_contract: Addr,
+    terraswap_factory: Addr,
+    short_reward_contract: string,
+): MirrorStakingConfig {
+    return {
+        owner: owner,
+        mirror_token: mirror_token,
+        mint_contract: mint_contract,
+        oracle_contract: oracle_contract,
+        terraswap_factory: terraswap_factory,
+        base_denom: "uusd",
+        premium_min_update_interval: 360,
+        short_reward_contract: short_reward_contract,
+    }
+}
+
+//=====================================================================================================================
+
+export interface MirrorCollateralOracleConfig{
+    owner: Addr,
+    mint_contract: Addr,
+    base_denom: string,
+    mirror_oracle: Addr,
+    anchor_oracle: Addr,
+    band_oracle: Addr,
+}
+
+export function MirrorCollateralOracleConfig(
+    owner: Addr,
+    mint_contract: Addr,
+    mirror_oracle: Addr,
+    anchor_oracle: Addr,
+    band_oracle: Addr,
+): MirrorCollateralOracleConfig {
+    return {
+        owner: owner,
+        mint_contract: mint_contract,
+        base_denom: "uusd",
+        mirror_oracle: mirror_oracle,
+        anchor_oracle: anchor_oracle,
+        band_oracle: band_oracle,
+    }
+}
+
+//=====================================================================================================================
+
+export interface MirrorFactoryConfig {
+    token_code_id: u64,
+    base_denom: String,
+    distribution_schedule: [u64, u64, Decimal][]
+}
+
+export function MirrorFactoryConfig(token_code_id: u64): MirrorFactoryConfig {
+    return {
+        token_code_id: token_code_id,
+        base_denom: "uusd",
+        distribution_schedule: [
+            [
+                21600,
+                31557600,
+                "54900000000000"
+            ],
+            [
+                31557600,
+                63093600,
+                "27450000000000"
+            ],
+            [
+                63093600,
+                94629600,
+                "13725000000000"
+            ],
+            [
+                94629600,
+                126165600,
+                "6862500000000"
+            ]
+        ],
     }
 }
--- src/integration_tests/deploy_mirror/definition.ts
+++ src/integration_tests/deploy_mirror/definition.ts
@@ -5,8 +5,16 @@ import {
     create_contract,
     instantiate_contract
 } from '../../utils';
-import {terraswap_factory_contract_addr, TokenConfig} from "../../config";
-import {MirrorTokenConfig, aUSTConfig} from "./config";
+import {TokenConfig} from "../../config";
+import {
+    MirrorCollateralOracleConfig,
+    MirrorCollectorConfig,
+    MirrorCommunityConfig, MirrorFactoryConfig,
+    MirrorGovConfig, MirrorLockConfig, MirrorMintConfig,
+    MirrorOracleConfig, MirrorStakingConfig,
+    MirrorTokenConfig
+} from "./config";
+import {anchor_init} from "../deploy_anchor/definition"
 
 //=============================================================================
 const artifacts_path = "wasm_artifacts";
@@ -26,20 +34,39 @@ const mirror_oracle_wasm = `${path_to_mirror_artifacts}/mirror_oracle.wasm`;
 const mirror_short_reward_wasm = `${path_to_mirror_artifacts}/mirror_short_reward.wasm`;
 const mirror_staking_wasm = `${path_to_mirror_artifacts}/mirror_staking.wasm`;
 
-export const cw20_contract_wasm = `${path_to_cosmwasm_artifacts}/cw20_base.wasm`;
+const cw20_contract_wasm = `${path_to_cosmwasm_artifacts}/cw20_base.wasm`;
 
 const terraswap_token_wasm = `${path_to_terraswap_artifacts}/terraswap_token.wasm`;
 const terraswap_factory_wasm = `${path_to_terraswap_artifacts}/terraswap_factory.wasm`;
 const terraswap_pair_wasm = `${path_to_terraswap_artifacts}/terraswap_pair.wasm`;
 
-
 //Steps
 //1. Store cw20_base
-//2. Store and instantiate terraswap_factory
+//2. Deploy terraswap_factory
 //3. Store terraswap_token
 //4. Instantiate mirror_token
-//5. Instantiate aUST_token
-
+//5. Deploy anchor market contracts
+//  5.1. get anchor_market_addr
+//  5.2. get aust_addr (aterra_token)
+//  5.1. get bluna_addr
+//6. Deploy mirror_gov
+//7. Deploy mirror_community
+//8. Deploy mirror_collector
+//9. Deploy mirror_oracle
+//10. Deploy mirror_mint
+//11. Deploy mirror_lock
+//12. Deploy mirror_staking
+//TODO: figure out how is short_reward_addr
+//13. Deploy mirror_factory
+//14. Setup mirror_factory (post_initialize)
+//15. Deploy mirror_collateral_oracle
+//TODO: figure out how is band_oracle
+//16. Assign mirror_factory to be the owner for mirror_oracle, mirror_staking and mirror_collector.
+//17. setup mirror_mint:
+//  17.1 assign mirror_factory to be the owner;
+//  17.2 set mirror_collateral_oracle;
+//  17.3 set mirror_staking
+//  17.4 set mirror_lock
 
 export async function mirror_init(lcd_client: LCDClient, sender: Wallet) {
 
@@ -52,10 +79,152 @@ export async function mirror_init(lcd_client: LCDClient, sender: Wallet) {
     const mirror_token_config = MirrorTokenConfig(sender.key.accAddress);
     const mirror_token_addr = await init_token(lcd_client, sender, terraswap_token_code_id, mirror_token_config);
 
-    const aust_token_config = aUSTConfig(sender.key.accAddress);
-    const aust_token_addr = await init_token(lcd_client, sender, cw20_code_id, aust_token_config);
+    const anchor_market_info = await anchor_init(lcd_client, sender);
+    const anchor_market_addr = anchor_market_info.contract_addr;
+    const aust_addr = anchor_market_info.aterra_token_addr;
+    const bluna_addr = anchor_market_info.bluna_token_addr;
+    const anchor_oracle_addr = anchor_market_info.oracle_addr;
 
+    const mirror_gov_config = MirrorGovConfig(mirror_token_addr);
+    const mirror_gov_addr = await create_contract(
+        lcd_client,
+        sender,
+        "mirror_gov",
+        mirror_gov_wasm,
+        mirror_gov_config
+    );
 
+    const mirror_community_config = MirrorCommunityConfig(sender.key.accAddress, mirror_token_addr);
+    const mirror_community_addr = await create_contract(
+        lcd_client,
+        sender,
+        "mirror_community",
+        mirror_community_wasm,
+        mirror_community_config
+    );
+
+    const mirror_collector_config = MirrorCollectorConfig(
+        sender.key.accAddress,  //owner will be changed to mirror_factory after its instantiation
+        mirror_gov_addr,
+        terraswap_factory_addr,
+        mirror_token_addr,
+        aust_addr,
+        anchor_market_addr,
+        bluna_addr
+    );
+    const mirror_collector_addr = await create_contract(
+        lcd_client,
+        sender,
+        "mirror_collector",
+        mirror_collector_wasm,
+        mirror_collector_config
+    );
+
+    const mirror_oracle_config = MirrorOracleConfig(sender.key.accAddress); //owner will be changed to mirror_factory after its instantiation
+    const mirror_oracle_addr = await create_contract(
+        lcd_client,
+        sender,
+        "mirror_oracle",
+        mirror_oracle_wasm,
+        mirror_oracle_config
+    );
+
+    const mirror_mint_config = MirrorMintConfig(
+        sender.key.accAddress,   //owner will be changed to mirror_factory after its instantiation
+        mirror_oracle_addr,
+        mirror_collector_addr,
+        sender.key.accAddress, // collateral_oracle will be changed to mirror_collateral_oracle after its instantiation
+        sender.key.accAddress, //staking will be changed to mirror_staking after its instantiation
+        terraswap_factory_addr,
+        sender.key.accAddress, // lock will be changed to mirror_lock after its instantiation
+        terraswap_token_code_id
+    );
+    const mirror_mint_addr = await create_contract(
+        lcd_client,
+        sender,
+        "mirror_mint",
+        mirror_mint_wasm,
+        mirror_mint_config
+    );
+
+    const mirror_lock_config = MirrorLockConfig(
+        sender.key.accAddress,
+        mirror_mint_addr
+    );
+    const mirror_lock_addr = await create_contract(
+        lcd_client,
+        sender,
+        "mirror_lock",
+        mirror_lock_wasm,
+        mirror_lock_config
+    );
+
+    const mirror_staking_config = MirrorStakingConfig(
+        sender.key.accAddress,   //owner will be changed to mirror_factory after its instantiation
+        mirror_token_addr,
+        mirror_mint_addr,
+        mirror_oracle_addr,
+        terraswap_factory_addr,
+        sender.key.accAddress // figure out how is short_reward_contract?
+    );
+    const mirror_staking_addr = await create_contract(
+        lcd_client,
+        sender,
+        "mirror_staking",
+        mirror_staking_wasm,
+        mirror_staking_config
+    );
+
+    const mirror_factory_config = MirrorFactoryConfig(cw20_code_id);
+    const mirror_factory_addr = await create_contract(
+        lcd_client,
+        sender,
+        "mirror_factory",
+        mirror_factory_wasm,
+        mirror_factory_config
+    );
+
+    await execute_contract(lcd_client, sender, mirror_factory_addr,
+        {
+            post_initialize: {
+                owner: sender.key.accAddress,
+                terraswap_factory: terraswap_factory_addr,
+                mirror_token: mirror_token_addr,
+                staking_contract: mirror_staking_addr,
+                oracle_contract: mirror_oracle_addr,
+                mint_contract: mirror_mint_addr,
+                commission_collector: mirror_collector_addr,
+            },
+        }
+    );
+
+    const mirror_collateral_oracle_config = MirrorCollateralOracleConfig(
+        mirror_factory_addr,     //owner will be changed to mirror_factory after its instantiation
+        mirror_mint_addr,
+        mirror_oracle_addr,
+        anchor_oracle_addr,
+        sender.key.accAddress
+    );
+    const mirror_collateral_oracle_addr = create_contract(
+        lcd_client,
+        sender,
+        "mirror_collateral_oracle",
+        mirror_collateral_oracle_wasm,
+        mirror_collateral_oracle_config
+    );
+
+    await assign_owner(lcd_client, sender, mirror_oracle_addr, mirror_factory_addr);
+    await assign_owner(lcd_client, sender, mirror_staking_addr, mirror_factory_addr);
+    await assign_owner(lcd_client, sender, mirror_collector_addr, mirror_factory_addr);
+
+    await execute_contract(lcd_client, sender, mirror_mint_addr, {
+        update_config: {
+            owner: mirror_factory_addr,
+            collateral_oracle: mirror_collateral_oracle_addr,
+            lock: mirror_lock_addr,
+            staking: mirror_staking_addr
+        },
+    });
 }
 
 async function TerraswapTokenCodeId(lcd_client: LCDClient, sender: Wallet): Promise<number> {
@@ -91,4 +260,14 @@ async function init_terraswap_factory(lcd_client: LCDClient, sender: Wallet, cw2
     let terraswap_factory_contract_addr = await instantiate_contract(lcd_client, sender, sender.key.accAddress, terraswap_factory_code_id, terraswap_factory_init_msg);
     console.log(`terraswap_factory instantiated\n\taddress: ${terraswap_factory_contract_addr}`);
     return terraswap_factory_contract_addr;
+}
+
+async function assign_owner(lcd_client: LCDClient, sender: Wallet, contract_addr: string, owner_addr: string) {
+    await execute_contract(lcd_client, sender, contract_addr,
+        {
+            update_config: {
+                owner: owner_addr,
+            },
+        }
+    );
 }
